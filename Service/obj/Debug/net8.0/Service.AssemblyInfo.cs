647d779a91513ce6bfcab2fbd614308055e3f969
ELCHUPACABRA01 <tabeeb788@gmail.com>
2024-10-16 15:27:27 +0200

    Seperate Create User UI

--- Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc8dd7bcd69ca4431efe285782adadca9ffd8790")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e83963cd906cbefc1803adb07fd020deaf32f40")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
