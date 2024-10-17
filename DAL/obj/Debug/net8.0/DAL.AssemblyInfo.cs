a070ddfbd80083c385d6b106b2ea3d011f1b96ea
Mira-Philippo <717862@student.inholland.nl>
2024-10-17 23:06:58 +0200

    Final merge

--- DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e83963cd906cbefc1803adb07fd020deaf32f40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+647d779a91513ce6bfcab2fbd614308055e3f969")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
